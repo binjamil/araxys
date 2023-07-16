d09b0944f0170dfd4291fa2b712bae5b9845aa79
Muhammad Bin Jamil <binjamil35@gmail.com>
2023-07-16 17:32:46 +0500

    Init

---
+++ src/araxys.config.ts
@@ -0,0 +1,16 @@
+import { Config } from "./config/types";
+
+const config: Config = {
+    collections: [
+        {
+            name: "Person",
+            fields: [
+                { name: "first_name", type: "Text" },
+                { name: "last_name", type: "Text" },
+                { name: "age", type: "Integer" },
+            ],
+        },
+    ],
+};
+
+export default config;
---
+++ src/config/types.ts
@@ -0,0 +1,20 @@
+export type Config = {
+    collections: Array<Collection>;
+};
+
+export type Collection = {
+    name: string;
+    fields: Array<Field>;
+};
+
+export type Field = {
+    name: string;
+    type: FieldType;
+};
+
+export const fieldTypes = {
+    Integer: 1,
+    Text: 2,
+} as const;
+
+export type FieldType = keyof typeof fieldTypes;
